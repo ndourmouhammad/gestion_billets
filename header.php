0e4635c96ddff3aff3bcf28379a2c71deb55b5b0
ndourmouhammad <ndourmouhammad15@gmail.com>
2024-03-05 12:35:49 +0000

    la page accueil

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>simplon travel agency</title>
+    <link rel="stylesheet" href="style/header-footer.css">
+    <link rel="stylesheet" href="style/index.css">
+</head>
+
+<body>
+    <header class="topbar">
+        <a class="logo" href="#"><img src="img/Logo.png" alt="logo-simplon-travel-agency"></a>
+        <nav class="topbar-nav">
+            <a href="index.php">Accueil</a>
+            <a href="create.php">Réservation</a>
+        </nav>
+    </header>
